8ad78aba52cd2a9d1bd60c30aea183e3cdc78011
Timothy Pfafman <tim@pfafman.com>
2015-03-17 05:47:58 -0700

    update to 1.7.1

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   summary: 'Nvd3.org charts with Meteor, updated version.',
-  version: "1.7.0_4",
+  version: "1.7.1",
   git: "https://github.com/pfafman/meteor-nvd3.git"
 });
 
 Package.on_use(function (api) {
   api.versionsFrom("METEOR@1.0");
-  api.use('d3js:d3@3.4.13', 'client');
+  api.use('d3js:d3', 'client');
 
   api.add_files('lib/nv.d3.js', 'client');
   api.add_files('lib/nv.d3.css', 'client');
