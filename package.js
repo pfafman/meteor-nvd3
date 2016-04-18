83ed889698fc6edb801eaf8e3f9614e59d5bf9c1
Timothy Pfafman <tim@pfafman.com>
2016-04-18 12:12:34 -0700

    version bump

--- package.js
+++ package.js
@@ -1,13 +1,14 @@
 Package.describe({
   name: "pfafman:nvd3",
   summary: 'Nvd3.org charts with Meteor, updated version.',
-  version: "1.8.1_3",
+  version: "1.8.2",
   git: "https://github.com/pfafman/meteor-nvd3.git"
 });
 
 Package.on_use(function (api) {
-  api.versionsFrom("METEOR@1.3");
+  api.versionsFrom("1.3.1");
 
+  api.use('modules@0.6.1');
   api.add_files('lib/nv.d3.js', 'client');
   api.add_files('lib/nv.d3.css', 'client');
 });
